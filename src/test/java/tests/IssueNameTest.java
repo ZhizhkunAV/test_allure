c44785525529150bfe15555202de2bf0428e62ae
Andrey <zizkun.av@list.ru>
2024-04-16 22:25:49 +0400

    Allure project

--- src/test/java/tests/IssueNameTest.java
+++ src/test/java/tests/IssueNameTest.java
@@ -1,14 +1,16 @@
 package tests;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
 
 public class IssueNameTest {
     @BeforeAll
@@ -23,11 +25,13 @@ public class IssueNameTest {
         SelenideLogger.addListener("allure", new AllureSelenide());
 
         open("/");
+        $(".header-search-input").click();
+        $("#query-builder-test").sendKeys("eroshenkoam/allure-example");
+        $("#query-builder-test").pressEnter();
 
-        $(".Header-old ").$$("li").findBy(text("Solutions")).hover().$(".HeaderMenu-dropdown").$$("li").find(text("Enterprise")).click();
-        $("#hero-section-brand-heading").shouldBe(text("The AI-powered\n" + "developer platform."));
 
-
+        $(linkText("eroshenkoam/allure-example")).click();
+        $("#issues-tab").click();
+        $(withText("#80")).should(Condition.exist);
     }
-
 }
---
+++ src/test/java/tests/StepsIssueTest.java
@@ -0,0 +1,55 @@
+package tests;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+import static org.openqa.selenium.By.linkText;
+
+public class StepsIssueTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://github.com";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    private static final String REPOSITORY = "eroshenkoam/allure-example";
+    private static final int ISSUE = 80;
+
+    @Test
+    void testLambdaTest() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+
+        step("Открываем главную страницу", () -> {
+            open("/");
+        });
+
+        step("Ищем репозиторий " + REPOSITORY, () -> {
+            $(".header-search-button").click();
+            $("#query-builder-test").sendKeys(REPOSITORY);
+            $("#query-builder-test").pressEnter();
+        });
+
+        step("Кликаем по ссылке репозитория " + REPOSITORY, () -> {
+            $(linkText(REPOSITORY)).click();
+        });
+
+        step("Кликаем на Issue", () -> {
+            $("#issues-tab").click();
+        });
+
+        step("Проверяем наличие Issue", () -> {
+                $(withText("#" + ISSUE)).should(Condition.exist);
+        });
+    }
+}
